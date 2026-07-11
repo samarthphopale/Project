8fd3352482c634a4e7910fcea85c556d3e82ebbd
samarthphopale <162269487+samarthphopale@users.noreply.github.com>
2026-07-11 08:47:53 -0700

    docker image working

--- docker/Dockerfile.java
+++ docker/Dockerfile.java
@@ -1,3 +1,5 @@
 FROM eclipse-temurin:21-jre
 COPY java-backend/v1/target/v1-0.0.1-SNAPSHOT.jar v1-0.0.1-SNAPSHOT.jar
+# ADD THIS LINE:
+EXPOSE 8000
 ENTRYPOINT ["java","-jar","/v1-0.0.1-SNAPSHOT.jar"]
